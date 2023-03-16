25ec6880c049a0d98b2c2f6a3f824a4d47993c79
Ansticefish <ansticefish0106@gmail.com>
2023-03-16 14:58:51 +0800

    Set up DataList layout and style

---
+++ src/components/DataLists.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+div.list(:class="{'clicked': openList}")
+  div.header
+    h1 打掃房間
+    div.btn(:class="{'clicked': openList}" @click="() => this.openList = !this.openList"
+      )
+  div.description(v-show="openList") rup wu0er ;lk;slrks;dlr
+
+</template>
+
+<script>
+export default {
+  name: 'DataList',
+  data () {
+    return {
+      openList: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list {
+  @include size (100%, fit-content);
+  border-bottom: 2px solid var(--list-border);
+  padding: 15px;
+  box-sizing: border-box;
+  transition: all $transition-time;
+  &.clicked {
+    background: var(--list-bg);
+  }
+  .header {
+    @include flex(row, space-between, center);
+    h1 {
+      @include h1;
+    }
+    .btn {
+      @include size ($height: 12px);
+      aspect-ratio: 1;
+      background-size: contain;
+      background-image: var(--add-btn);
+      @include divBtn;
+      &:hover, &.clicked {
+        transform: rotate(45deg);
+      }
+    }
+  }
+  .description {
+    margin-top: 10px;
+  }
+}
+</style>
