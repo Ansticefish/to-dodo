e5b63964b22410048e3a8fe8fd57681871704b67
Ansticefish <ansticefish0106@gmail.com>
2023-03-16 17:35:04 +0800

    Set up firebase hosting

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import store from './store'
 // import shared scss
 import "@/scss/theme.scss"
 import axios from 'axios'
+// set up firebase
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAm8ni1uX1XdmcwN4VG3y60atR7VFfPsa0",
+  authDomain: "to-dodo-6ceb2.firebaseapp.com",
+  projectId: "to-dodo-6ceb2",
+  storageBucket: "to-dodo-6ceb2.appspot.com",
+  messagingSenderId: "981751867977",
+  appId: "1:981751867977:web:9d6338d4b742de26aeed98",
+  measurementId: "G-GZH1FGRGX1"
+};
+const firebase = initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
@@ -15,5 +27,6 @@ new Vue({
   router,
   store,
   axios,
+  firebase,
   render: h => h(App)
 }).$mount('#app')
